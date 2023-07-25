61c3bb05492ed57e9f2741320d617ef77a88e756
Juan Cruz <juancruzliberati@hotmail.com>
2023-07-25 02:49:27 -0300

    Initial commit

---
+++ app/Http/Controllers/ExampleController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class ExampleController extends Controller
+{
+    //
+
+    public function index(){
+
+        $user=Auth::user();
+
+   return view('example',compact('user'));
+
+
+    }
+}
